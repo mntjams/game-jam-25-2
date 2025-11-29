1ebccb52e2753e04d755c129f05a3ce27dfba6e8
Nekaque <3217cody@gmail.com>
2025-11-29 16:56:34 +0100

    volume slider added

--- scripts/audio_settings.gd
+++ scripts/audio_settings.gd
@@ -1,9 +1,9 @@
 extends Control
 
-
+var normalization = 32
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$HUD/SFX/SFXSlider.value = AudioServer.get_bus_volume_linear(0)*normalization
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +13,7 @@ func _process(_delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("settings"):
 		$HUD.visible = !$HUD.visible
+
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(0, value/normalization)
