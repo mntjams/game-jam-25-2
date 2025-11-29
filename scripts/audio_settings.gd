082b2a184881df94c8b79b28458c7e5711053807
Nekaque <3217cody@gmail.com>
2025-11-29 17:56:31 +0100

    settings stop game

--- scripts/audio_settings.gd
+++ scripts/audio_settings.gd
@@ -3,7 +3,7 @@ extends Control
 var normalization = 32
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$HUD/SFX/SFXSlider.value = AudioServer.get_bus_volume_linear(0)*normalization
+	$HUD/SFXSlider.value = AudioServer.get_bus_volume_linear(0)*normalization
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,6 +13,7 @@ func _process(_delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("settings"):
 		$HUD.visible = !$HUD.visible
+		get_tree().paused = !get_tree().paused
 
 
 func _on_sfx_slider_value_changed(value: float) -> void:
