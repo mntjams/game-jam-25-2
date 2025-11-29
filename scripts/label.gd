dc9e717d230e7e3fd342e39cd6fa5a10fa23f74f
Nekaque <3217cody@gmail.com>
2025-11-29 14:43:09 +0100

    hud done

--- scripts/label.gd
+++ scripts/label.gd
@@ -9,3 +9,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_player_no_woman_in_sight() -> void:
+	visible = false
+
+
+	
+
+
+func _on_player_woman_entered_sight() -> void:
+	visible = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 var women_in_sight : Array[InteractableWoman] = []
 var movement_speed: float = 50
 
+signal woman_entered_sight
+signal no_woman_in_sight
+
 func _ready() -> void:
 	for woman in get_tree().get_nodes_in_group("NPCs"):
 		woman.connect("approached_by_player", _on_approach)
@@ -13,11 +16,14 @@ func _ready() -> void:
 func _on_approach(woman : InteractableWoman):
 	women_in_sight.append(woman)
 	print(women_in_sight)
+	emit_signal("woman_entered_sight")
 	
 func _on_leave(woman : InteractableWoman):
 	var where = women_in_sight.find(woman)
 	women_in_sight.remove_at(where)
 	print(women_in_sight)
+	if len(women_in_sight) == 0:
+		emit_signal("no_woman_in_sight")
 	
 	
 
