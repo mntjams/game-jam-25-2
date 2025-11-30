43423cb11e16408efe1e89395911c7998719d52e
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-11-30 12:40:05 +0100

    Integrate bar minigame

--- scripts/bar_minigame.gd
+++ scripts/bar_minigame.gd
@@ -2,10 +2,20 @@ extends Node2D
 @onready var pathf = $CanvasLayer/Path2D/PathFollow2D
 @onready var left = $CanvasLayer/Path2D/left
 @onready var right = $CanvasLayer/Path2D/right
+
 var vel = 0.002
 var press_vel = vel*30
 var sweet_spot = 0
 var lost = false
+var won = false
+
+signal finished(success: bool, interest_gained : float)
+
+@onready var win_timer = $WinTimer
+var lost_reward = -10
+var win_reward = 10
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pathf.progress_ratio = 0.5
@@ -13,11 +23,16 @@ func _ready() -> void:
 	var margin = (1-sweet_spot)/2
 	left.progress_ratio = margin
 	right.progress_ratio = 1-margin
-	
+	win_timer.start()
+
+func start():
+	pass
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
-	if not lost:
+func _physics_process(_delta: float) -> void:
+	if not lost and not won:
 		if Input.is_action_just_pressed("left"):
 			pathf.progress_ratio -= press_vel
 		elif Input.is_action_just_pressed("right"):
@@ -37,3 +52,13 @@ func losing():
 	if pathf.progress_ratio < left.progress_ratio:
 		tween.tween_property(pathf, "progress_ratio", 0,1)
 	else: tween.tween_property(pathf, "progress_ratio", 1,1)
+	await tween.finished
+	emit_signal("finished",false,lost_reward)
+	queue_free()
+
+
+func _on_win_timer_timeout():
+	# TODO: win sound effect
+	won = true
+	emit_signal("finished",true,win_reward)
+	queue_free()
