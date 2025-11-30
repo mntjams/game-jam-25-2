96a489292d34104dae1881c28462d6aabf6920ce
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-11-30 11:07:23 +0100

    Make interest bar show red if lost interest

--- scripts/test_minigame.gd
+++ scripts/test_minigame.gd
@@ -2,10 +2,12 @@ extends CanvasLayer
 
 class_name MiniGame
 
+var interest_reward : float = 100
+
 signal finished(success: bool, interest_gained : float)
 
 func start() -> void:
 	await get_tree().create_timer(0.01).timeout
 	# print("test game finished")
-	emit_signal("finished", true, 100.0)
+	emit_signal("finished", true, interest_reward)
 	queue_free()
