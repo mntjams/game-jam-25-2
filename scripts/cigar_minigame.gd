e8eb47fc3b107b2b72abddbfd24f50c8cebfcb3f
Nekaque <3217cody@gmail.com>
2025-11-30 14:28:56 +0100

    cigar polished

--- scripts/cigar_minigame.gd
+++ scripts/cigar_minigame.gd
@@ -6,7 +6,7 @@ extends Node2D
 @onready var particles = $CanvasLayer/full/filter/not_filter/CPUParticles2D
 @onready var label = $CanvasLayer/LabelLabel
 var finished_game = false
-var vel = 30
+var vel = 45
 var tween = null
 var origin_pos = 0
 # Called when the node enters the scene tree for the first time.
@@ -32,7 +32,7 @@ func _ready() -> void:
 	good.size.x -= margin
 	good.position.x += margin/3
 	tween = get_tree().create_tween()
-	tween.tween_property(not_filter, "size", Vector2(0,not_filter.size.y), 5)
+	tween.tween_property(not_filter, "size", Vector2(49,not_filter.size.y), 5)
 	tween.tween_callback(_won)
 	filter.position.x = good.position.x +1
 	origin_pos = not_filter.size.x
@@ -40,26 +40,27 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	var left_part = filter.position.x
-	var right_part = left_part+filter.size.x
-	if left_part < good.position.x and not finished_game:
-		# print("Die")
-		_lost()
-	elif right_part > good.position.x+good.size.x:
-		# print("Not smoking")
-		tween.pause()
-	elif not tween.is_running() and not finished_game:
-		tween.play()
-	
-	var pos = filter.position
-	if Input.is_action_just_pressed("slots"): pos.x-=vel
-	else:
-		pos.x+=vel/15.0
-		if right_part >= full.size.x:
-				return
-	var move_tween = get_tree().create_tween()
-	move_tween.tween_property(filter,"position", pos, delta)
-	particles.position.x = not_filter.size.x
+	if not finished_game:
+		var left_part = filter.position.x
+		var right_part = left_part+filter.size.x
+		if left_part < good.position.x and not finished_game:
+			# print("Die")
+			_lost()
+		elif right_part > good.position.x+good.size.x:
+			# print("Not smoking")
+			tween.pause()
+		elif not tween.is_running() and not finished_game:
+			tween.play()
+		
+		var pos = filter.position
+		if Input.is_action_just_pressed("slots"): pos.x-=vel
+		else:
+			pos.x+=vel/20.0
+			if right_part >= full.size.x:
+					return
+		var move_tween = get_tree().create_tween()
+		move_tween.tween_property(filter,"position", pos, delta)
+		particles.position.x = not_filter.size.x
 
 func _won():
 	finished_game = true
